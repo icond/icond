461d1a0ba5ed1b6970ec52a9c3065673c68ef9c2
David Alexandre <skupiie@gmail.com>
2016-05-04 03:58:10 -0700

    More files added, and more
    More files, such as our own css file, img folder created, and a default
    navbar.

--- index.php
+++ index.php
@@ -3,11 +3,39 @@
     <head>
         <title>icond</title>
         <meta charset="UTF-8">
+        <!--css-->
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <!--js-->
+        <!--adicionar aqui jquery-->        
         <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
     </head>
     <body>
-    
+        <!--header-->
+        <nav class="navbar navbar-inverse">
+          <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span> 
+                </button>
+                <a class="navbar-brand" href="#"><img src="img/icond_v2.png" class="logo-img"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Teste 1</a></li>
+                <li><a href="#">Teste 2</a></li> 
+                <li><a href="#">Teste 3</a></li> 
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+            </div>
+            </div>
+        </nav>
     </body>
 </html>
