cc5ea30cd47923065829a58eb9441ecc93212385
David Alexandre <skupiie@gmail.com>
2016-05-05 15:34:41 +0100

    Design Update - 10% completed
    nav updated - needs lots of work

--- index.php
+++ index.php
@@ -5,37 +5,48 @@
         <meta charset="UTF-8">
         <!--css-->
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="css/main.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
+       <!--jquery-->
+        <script   src="https://code.jquery.com/jquery-2.2.3.js"   integrity="sha256-laXWtGydpwqJ8JA+X9x2miwmaiKhn8tVmOVEigRNtP4="   crossorigin="anonymous"></script>
         <!--js-->
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
         <!--adicionar aqui jquery-->        
         <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
     </head>
     <body>
         <!--header-->
-        <nav class="navbar navbar-inverse">
-          <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span> 
-                </button>
-                <a class="navbar-brand" href="#"><img src="img/icond_v2.png" class="logo-img"></a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Teste 1</a></li>
-                <li><a href="#">Teste 2</a></li> 
-                <li><a href="#">Teste 3</a></li> 
-              </ul>
-              <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                </ul>
-            </div>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                    </button>
+                    <!--<a class="navbar-brand" href="#">WebSiteName</a>-->
+                    <img src="img/icond.ico" class="navbar-left logo-img img-away-left"><p class="navbar-text navbar-text-aux hidden-xs"><b>Gestão Condominios Online</b></p>
+                </div>
+                <!--codigo para fazer login etc -->
+                <!--<div class="collapse navbar-collapse" id="myNavbar">
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                  </ul>
+                </div>-->
+                <!--codigo para utilizador com sessão-->
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><p class="navbar-text-data"><span style="color: #0071BC;"><b>Utilizador</span>: Admin</p></b></li>
+                        <li><p class="navbar-text-data"><span style="color: #0071BC;"><b>Condominio</span>: 00039</p></b></li>
+                        <li><p class="navbar-text-data">
+                            <a href="#" class="myExitButton">Sair</a></p></li>
+                    </ul>
+                </div>
             </div>
         </nav>
+        <div class="navmenu">
+            <ul class="nav nav-pills">
+                <li class="active"><a href="#">Regular link</a></li>
+            </ul>
+        </div>
     </body>
 </html>
