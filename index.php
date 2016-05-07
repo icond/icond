8234aa8a43868e0f37c5fd9e3a950f111e94a90d
Skupiie <skupiie@gmail.com>
2016-05-07 22:51:52 +0100

    index.php updated

--- index.php
+++ index.php
@@ -85,5 +85,34 @@
                 <span class="glyphicon glyphicon-chevron-right"></span>
             </a>
         </div>
+        <!--icons destaque-->
+        <div class="container-fluid">
+            <div class="row icons">
+                <div class="col-lg-4 col-md-4 col-sm-12 text-center">
+                    <span class="glyphicon glyphicon-signal highlight-icon"></span>
+                    <span class="iconTitle">Sem Instalações</span>
+                    <br />
+                    <br />
+                    <br />
+                    <p class="container-fluid text-justify iconTextSpace">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 text-center">
+                    <span class="glyphicon glyphicon-cog highlight-icon"></span>
+                    <span class="iconTitle">Sem Instalações</span>
+                    <br />
+                    <br />
+                    <br />
+                    <p class="container-fluid text-justify iconTextSpace">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-12 text-center">
+                    <span class="glyphicon glyphicon-refresh highlight-icon"></span>
+                    <span class="iconTitle">Sem Instalações</span>
+                    <br />
+                    <br />
+                    <br />
+                    <p class="container-fluid text-justify iconTextSpace">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
