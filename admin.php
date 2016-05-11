d432804f278aa92e70387dc28cd2b8eaacc44479
Hmortishu <Hmortishu@gmail.com>
2016-05-11 08:32:14 +0100

    Update da estrutura

--- admin.php
+++ admin.php
@@ -108,29 +108,33 @@
                 <div class="panel panel-default">
                     <div class="panelheading"><h3>Bem vindo</h3></div>
                     <div class="panel-body">
-                        <div class="adminleft">
-                            <p><b>Dados Condominio</b></p>
-                            nif: 269009540<br>
-                            nome: condominio rua X<br>
-                            morada: rua x 1100 Lx<br><br>
-                        </div>
-                        <div class="adminright">
-                            cenas cenas
-                            asdasdda
-                            asdasdasdadas
-                            asdasdasdasdasd
-                            asdasdasdadasd
-                            asdasdasdasd
-                            asdasdasdasdasds
-                            asdasdasdasdas
-                            asdasdasdasd
-                        </div>
-                        <div class="adminleft">
-                            <p><b>Dados Administrador</b></p>
-                            nome: Almerindo<br>
-                            tel: 129999999<br>
-                            tel: 219999999<br>
-                            email: cenas@cenas.pt
+                        <div id="row">
+                            <div class="col-lg-8 col-md-6 col-sm-6">
+                                <div>
+                                    <p><b>Dados Condominio</b></p>
+                                    nif: 269009540<br>
+                                    nome: condominio rua X<br>
+                                    morada: rua x 1100 Lx<br><br>
+                                </div>
+                                <div>
+                                    <p><b>Dados Administrador</b></p>
+                                    nome: Almerindo<br>
+                                    tel: 129999999<br>
+                                    tel: 219999999<br>
+                                    email: cenas@cenas.pt
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-5 col-sm-7">
+                                <p><b>Informações extra</b></p>
+                                Coisa partida<br>
+                                Festa no 3ºEsq<br>
+                                asdasdasdasdasd<br>
+                                asdasdasdadasd<br>
+                                asdasdasdasd<br>
+                                asdasdasdasdasds<br>
+                                asdasdasdasdas<br>
+                                asdasdasdasd<br>
+                            </div>
                         </div>
                     </div>
                 </div>
