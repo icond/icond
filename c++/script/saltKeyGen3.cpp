7b1acf1128f39c8478580636585cb17ba3d3a2ad
Skupiie <skupiie@gmail.com>
2016-06-10 15:55:46 +0100

    final c++ update

---
+++ c++/script/saltKeyGen3.cpp
@@ -0,0 +1,44 @@
+/*
+ * Author: David Alexandre Lopes
+ * Date: 10/06/2016
+ *
+ * What is it supposed to do?
+ * It generates a 18 length string according to 2 given numbers
+ *
+ */
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+//tamanho da string por defeito
+const short _SALTLEN = 18;
+
+int main(int argc, char * argv[])
+{
+    if(argc != 3)
+    {
+        std::cout << "ERROR";
+        return 1;
+    }
+    std::string salt = "";
+    long long foo = (long long)atoi(argv[1]);
+    long long bar = (long long)atoi(argv[2]);
+
+    //roda duas vezes com dois padrões diferentes
+    for(int i = 0; i < _SALTLEN; i++)
+    {
+        char ch = (foo * bar) % 26;
+        ch += 97;
+        salt += ch;
+        foo += bar;
+    }
+    for(int i = 0; i < _SALTLEN; i++)
+    {
+        char ch = (foo * bar) % 26;
+        ch += 97;
+        salt[i] = (char)ch;
+        bar += foo;
+    }
+    std::cout << salt;
+    return 0;
+}
--- cppTeste.php
+++ cppTeste.php
@@ -1,7 +1,7 @@
 <?php
 	$um = 2;
 	$dois = 4;
-	echo exec('C:\xampp\htdocs\icond\c++\script\saltKeyGen2 2 4') . "<br>";
-	echo exec('C:\xampp\htdocs\icond\c++\script\saltKeyGen2 4 8') . "<br>";
-	echo exec('C:\xampp\htdocs\icond\c++\script\saltKeyGen2 200 1238');
+	echo exec('C:\xampp\htdocs\icond\c++\script\saltKeyGen3 4 2') . "<br>";
+	echo exec('C:\xampp\htdocs\icond\c++\script\saltKeyGen3 4 8') . "<br>";
+	echo exec('C:\xampp\htdocs\icond\c++\script\saltKeyGen3 200 1238');
 ?>
