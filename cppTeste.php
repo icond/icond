00a90b4ec00862ac0057acd51f41caf2ecf3148f
David Alexandre <skupiie@gmail.com>
2016-06-17 11:51:49 +0100

    updates

--- cppTeste.php
+++ cppTeste.php
@@ -1,5 +1,5 @@
 <?php
-    include 'security/functions.php';
+    include dirname(__FILE__) . '/security/functions.php';
     echo textCrypt("password") . "";
     echo "<br /><br />" . passwordHash(textCrypt("password"));
 
