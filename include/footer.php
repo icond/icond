397734baf8f6d341f20e8c07000aa54d9043e1de
duusk <marco_rafael97@outlook.com>
2016-06-30 10:02:32 +0100

    footerrz and stuff about imgs

--- include/footer.php
+++ include/footer.php
@@ -1,31 +1,43 @@
-<footer>
+<footer class="opensans">
 
+	<div class="row">
+		<div class="col-lg-4 col-md-4 col-sm-12 text-center marginTOP">
+			<label class="">Contactos</label>
+			<br>
+			<hr class="style14">
+			<br>
+			<div style="width:60%; margin:auto; text-align:left;">
+				Telefone: +351 212 564 862 <br>
+				Email: icond@condominios.pt <br>
+				Skype: iCond
+			</div>
+		</div>
 
+		<div class="col-lg-4 col-md-4 col-sm-12 text-center marginTOP">
+			<label>Siga-nos</label>
+			<br>
+			<hr class="style14">
+			<br>
+			<div style="width:60%; margin:auto; text-align:left;">
+				<a href="http://www.google.pt" target="_blank"><span class="brandico-facebook-rect"> Facebook</span></a> <br>
+				<a href="http://www.google.pt" target="_blank"><span class="brandico-googleplus-rect"> Google+</span></a> <br>
+				<a href="http://www.google.pt" target="_blank"><span class="brandico-linkedin-rect"> LinkedIn</span></a> <br>
+			</div>
+		</div>
 
-<div class="col-lg-4 col-md-4 col-sm-12 text-center marginTOP">
-	<label>Contactos</label><br><br>
-<div style="width:60%; margin:auto; text-align:left;">
-	Telefone: +351 212 564 862 <br>
-	Email: icond@condominios.pt <br>
-	Skype: iCond</div>
-</div>
+		<div class="col-lg-4 col-md-4 col-sm-12 text-center marginTOP">
+			<label>Sobre nós</label>
+			<br>
+			<hr class="style14">
+			<br>
+			<div style="width:60%; margin:auto; text-align:left;" class="text-justify">
+				Quisque sodales ut elit sed pharetra. 
+				Donec euismod at mi ut auctor. 
+				Ut bibendum.
+			</div>
+		</div>
+	</div>
 
-<div class="col-lg-4 col-md-4 col-sm-12 text-center marginTOP">
-	<label>Contactos</label><br><br>
-<div style="width:60%; margin:auto; text-align:left;">
-	Telefone: +351 212 564 862 <br>
-	Email: icond@condominios.pt <br>
-	Skype: iCond</div>
-</div>
-
-<div class="col-lg-4 col-md-4 col-sm-12 text-center marginTOP">
-	<label>Contactos</label><br><br>
-<div style="width:60%; margin:auto; text-align:left;">
-	Telefone: +351 212 564 862 <br>
-	Email: icond@condominios.pt <br>
-	Skype: iCond</div>
-</div>
-<div style="width:100%; text-align:center; position:fixed; bottom:0;">iCond&copy; 2016</div>
-
+	<div style="width:100%; text-align:center; position:fixed; bottom:0;">iCond&copy; 2016</div>
 
 </footer>
