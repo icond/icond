638252d389f0fe2ee3caf7500da1b8b7363d3048
Sandro Milhano <sandromilhano@gmail.com>
2016-08-23 16:15:17 +0100

    Criada a BD e alterado BD.txt

---
+++ icond.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 23, 2016 at 05:14 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `icond`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `apartamentos`
+--
+
+CREATE TABLE `apartamentos` (
+  `idApartamento` int(11) NOT NULL,
+  `andar` int(11) NOT NULL,
+  `letra` varchar(255) NOT NULL,
+  `nPredio` int(11) NOT NULL,
+  `idCond` int(11) NOT NULL,
+  `idParcela` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `condominios`
+--
+
+CREATE TABLE `condominios` (
+  `idCond` int(11) NOT NULL,
+  `morada` varchar(255) NOT NULL,
+  `lote` varchar(255) NOT NULL,
+  `codigoPostal` varchar(255) NOT NULL,
+  `localidade` varchar(255) NOT NULL,
+  `cidade` varchar(255) NOT NULL,
+  `idPais` int(11) NOT NULL,
+  `nifCond` varchar(255) NOT NULL,
+  `nAndares` int(11) NOT NULL,
+  `ibanCond` int(11) NOT NULL,
+  `idEmpresa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `despesas`
+--
+
+CREATE TABLE `despesas` (
+  `idDespesa` int(11) NOT NULL,
+  `idCond` int(11) NOT NULL,
+  `descricao` text NOT NULL,
+  `valor` int(11) NOT NULL,
+  `data` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pagamentos`
+--
+
+CREATE TABLE `pagamentos` (
+  `idPagamento` int(11) NOT NULL,
+  `quantia` int(11) NOT NULL,
+  `data` date NOT NULL,
+  `idParcela` int(11) NOT NULL,
+  `isPaid` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pais`
+--
+
+CREATE TABLE `pais` (
+  `idPais` int(11) NOT NULL,
+  `pais` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parcelas`
+--
+
+CREATE TABLE `parcelas` (
+  `idParcela` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `isAdmin` tinyint(4) NOT NULL,
+  `codigo` varchar(255) NOT NULL,
+  `idCond` int(11) NOT NULL,
+  `nifParcela` int(11) NOT NULL,
+  `andar` int(11) NOT NULL,
+  `comissaoMensal` int(11) NOT NULL,
+  `organizacao` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `apartamentos`
+--
+ALTER TABLE `apartamentos`
+  ADD PRIMARY KEY (`idApartamento`);
+
+--
+-- Indexes for table `condominios`
+--
+ALTER TABLE `condominios`
+  ADD PRIMARY KEY (`idCond`);
+
+--
+-- Indexes for table `despesas`
+--
+ALTER TABLE `despesas`
+  ADD PRIMARY KEY (`idDespesa`);
+
+--
+-- Indexes for table `pagamentos`
+--
+ALTER TABLE `pagamentos`
+  ADD PRIMARY KEY (`idPagamento`);
+
+--
+-- Indexes for table `pais`
+--
+ALTER TABLE `pais`
+  ADD PRIMARY KEY (`idPais`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `apartamentos`
+--
+ALTER TABLE `apartamentos`
+  MODIFY `idApartamento` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `condominios`
+--
+ALTER TABLE `condominios`
+  MODIFY `idCond` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `despesas`
+--
+ALTER TABLE `despesas`
+  MODIFY `idDespesa` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pagamentos`
+--
+ALTER TABLE `pagamentos`
+  MODIFY `idPagamento` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pais`
+--
+ALTER TABLE `pais`
+  MODIFY `idPais` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
