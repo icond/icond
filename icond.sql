110f77ea934a9730e8a0a0cc11fb86081d39f0b8
David Alexandre <skupiie@gmail.com>
2016-09-08 10:52:05 +0100

    nome da parcela updated

--- icond.sql
+++ icond.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 27, 2016 at 06:28 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.6
+-- Host: localhost
+-- Generation Time: 08-Set-2016 às 11:50
+-- Versão do servidor: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `apartamentos`
+-- Estrutura da tabela `apartamentos`
 --
 
 CREATE TABLE `apartamentos` (
@@ -38,7 +38,7 @@ CREATE TABLE `apartamentos` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `condominios`
+-- Estrutura da tabela `condominios`
 --
 
 CREATE TABLE `condominios` (
@@ -55,10 +55,17 @@ CREATE TABLE `condominios` (
   `idEmpresa` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `condominios`
+--
+
+INSERT INTO `condominios` (`idCond`, `morada`, `lote`, `codigoPostal`, `localidade`, `cidade`, `idPais`, `nifCond`, `nAndares`, `ibanCond`, `idEmpresa`) VALUES
+(1, 'Rua atec', '5', '2500-300', 'Lisboa', 'Lisboa', 1, '912312393', 5, 0, 0);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `despesas`
+-- Estrutura da tabela `despesas`
 --
 
 CREATE TABLE `despesas` (
@@ -72,7 +79,7 @@ CREATE TABLE `despesas` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pagamentos`
+-- Estrutura da tabela `pagamentos`
 --
 
 CREATE TABLE `pagamentos` (
@@ -86,7 +93,7 @@ CREATE TABLE `pagamentos` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pais`
+-- Estrutura da tabela `pais`
 --
 
 CREATE TABLE `pais` (
@@ -97,11 +104,12 @@ CREATE TABLE `pais` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `parcelas`
+-- Estrutura da tabela `parcelas`
 --
 
 CREATE TABLE `parcelas` (
   `idParcela` int(11) NOT NULL,
+  `full_name` text NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `isAdmin` tinyint(4) NOT NULL,
@@ -112,6 +120,37 @@ CREATE TABLE `parcelas` (
   `comissaoMensal` int(11) NOT NULL,
   `organizacao` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `parcelas`
+--
+
+INSERT INTO `parcelas` (`idParcela`, `full_name`, `email`, `password`, `isAdmin`, `codigo`, `idCond`, `nifParcela`, `andar`, `comissaoMensal`, `organizacao`) VALUES
+(1, '', '', '', 0, 'zN5c5', 1, 0, 1, 0, 'Esquerdo'),
+(2, '', '', '', 0, 'maN5c', 1, 0, 1, 0, 'Direito'),
+(3, '', '', '', 0, 'hLfhx', 1, 0, 1, 0, 'Frente'),
+(4, '', '', '', 0, 'Vtnq4', 1, 0, 1, 0, 'Frente'),
+(5, '', '', '', 0, 'tCuEU', 1, 0, 1, 0, 'Frente'),
+(6, '', '', '', 0, 'AX9y9', 1, 0, 2, 0, 'Esquerdo'),
+(7, '', '', '', 0, 'J5VNf', 1, 0, 2, 0, 'Direito'),
+(8, '', '', '', 0, 'Y7rMb', 1, 0, 2, 0, 'Frente'),
+(9, '', '', '', 0, 'B3VPk', 1, 0, 2, 0, 'Frente'),
+(10, '', '', '', 0, 'sLLM8', 1, 0, 2, 0, 'Frente'),
+(11, '', '', '', 0, 'OdJfQ', 1, 0, 3, 0, 'Esquerdo'),
+(12, '', '', '', 0, 'DPOLm', 1, 0, 3, 0, 'Direito'),
+(13, '', '', '', 0, 'VurQe', 1, 0, 3, 0, 'Frente'),
+(14, '', '', '', 0, 'FOm4z', 1, 0, 3, 0, 'Frente'),
+(15, '', '', '', 0, 'xDBst', 1, 0, 3, 0, 'Frente'),
+(16, '', '', '', 0, 'TQbEC', 1, 0, 4, 0, 'Esquerdo'),
+(17, '', '', '', 0, 'jsO2G', 1, 0, 4, 0, 'Direito'),
+(18, '', '', '', 0, 'DDvsn', 1, 0, 4, 0, 'Frente'),
+(19, '', '', '', 0, 'PmOda', 1, 0, 4, 0, 'Frente'),
+(20, '', '', '', 0, '3RXpT', 1, 0, 4, 0, 'Frente'),
+(21, '', '', '', 0, 'xSx6h', 1, 0, 5, 0, 'Esquerdo'),
+(22, '', '', '', 0, 'WY87C', 1, 0, 5, 0, 'Direito'),
+(23, '', '', '', 0, 'Jr3xr', 1, 0, 5, 0, 'Frente'),
+(24, '', '', '', 0, 'H93bz', 1, 0, 5, 0, 'Frente'),
+(25, '', '', '', 0, 'pYSbb', 1, 0, 5, 0, 'Frente');
 
 --
 -- Indexes for dumped tables
@@ -166,7 +205,7 @@ ALTER TABLE `apartamentos`
 -- AUTO_INCREMENT for table `condominios`
 --
 ALTER TABLE `condominios`
-  MODIFY `idCond` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idCond` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `despesas`
 --
@@ -186,7 +225,7 @@ ALTER TABLE `pais`
 -- AUTO_INCREMENT for table `parcelas`
 --
 ALTER TABLE `parcelas`
-  MODIFY `idParcela` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idParcela` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
